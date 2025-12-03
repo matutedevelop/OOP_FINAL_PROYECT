3efc3a441f20a4601617b95dec5f288cfa70bd3d
Santiago Ayon <santiayosan2005@gmail.com>
2025-12-03 05:50:07 -0600

    Clase Matrix implementada

--- lib/src/linearalgebra/Matrix.java
+++ lib/src/linearalgebra/Matrix.java
@@ -1,25 +1,233 @@
 package linearalgebra;
 
-public class Matrix {
+
+public class Matrix<T extends Number> {
 
     private int ncols;
     private int nrows;
 
-    public Number[][] values;
+    private Number[][] values;
 
-    public Matrix() {
+    // constructores
+    public Matrix(int nrows, int ncols) {
+        // Validación: Si filas O columnas son menores o iguales a 0
+        if (nrows <= 0 || ncols <= 0) {
+            throw new IllegalArgumentException("Las dimensiones deben ser mayores a 0. Recibido: " + nrows + " x " + ncols);
+        }
+
+        this.nrows = nrows;
+        this.ncols = ncols;
+        this.values = new Number[nrows][ncols];
 
     }
 
+    public Matrix(T[][] values) {
 
-    public boolean equals(){
-        //TODO hacer esta madre
-        return false;
+        if (values == null || values.length == 0 || values[0].length == 0) {
+            throw new IllegalArgumentException("El array de datos no puede ser nulo ni estar vacío.");
+        }
+
+        this.nrows = values.length;
+        this.ncols = values[0].length;
+        this.values = new Number[nrows][ncols];
+        
+        // Copia profunda para evitar referencias externas
+        for (int i = 0; i < nrows; i++) {
+            for (int j = 0; j < ncols; j++) {
+
+                setValue(i, j, values[i][j]);
+            }
+        }
+
     }
 
+    // setter y getters
+
+    public double getValue(int row, int col) {
+        validarIndices(row, col);
+        return this.values[row][col].doubleValue();
+    }
+
+    public void setValue(int row, int col, T valor) {
+        validarIndices(row, col);
+        this.values[row][col] = valor;
+    }
+
+    public int getNcols() {
+        return ncols;
+    }
+
+    public int getNrows() {
+        return nrows;
+    }
+
+    public int[] getShape() {
+        int[] shape = {getNrows(), getNcols()};
+        return shape;
+    }
+
+
+    // OPERACIOENS BASICAS DE LAS MATRICES
+
+    public Matrix<Double> sumar(Matrix<? extends Number> matriz) {
+        if (this.nrows != matriz.nrows || this.ncols != matriz.ncols) {
+            throw new IllegalArgumentException("Las dimensiones deben ser iguales para sumar.");
+        }
+
+        Matrix<Double> resultado = new Matrix<>(this.nrows, this.ncols);
+
+        for (int i = 0; i < this.nrows; i++) {
+            for (int j = 0; j < this.ncols; j++) {
+                
+                double valA = this.values[i][j] == null ? 0.0 : this.values[i][j].doubleValue();
+                double valB = matriz.values[i][j] == null ? 0.0 : matriz.values[i][j].doubleValue();
+
+                resultado.setValue(i, j, valA + valB);
+            }
+        }
+
+        return resultado;
+    } // fin de la suma 
+
+
+    public Matrix<Double> restar(Matrix<? extends Number> matriz) {
+        if (this.nrows != matriz.nrows || this.ncols != matriz.ncols) {
+            throw new IllegalArgumentException("Las dimensiones deben ser iguales para sumar.");
+        }
+
+        Matrix<Double> resultado = new Matrix<>(this.nrows, this.ncols);
+
+        for (int i = 0; i < this.nrows; i++) {
+            for (int j = 0; j < this.ncols; j++) {
+                
+                double valA = this.values[i][j] == null ? 0.0 : this.values[i][j].doubleValue();
+                double valB = matriz.values[i][j] == null ? 0.0 : matriz.values[i][j].doubleValue();
+
+                resultado.setValue(i, j, valA - valB);
+            }
+        }
+
+        return resultado;
+    } // fin de la resta
+
+
+    public Matrix<Double> multiplicar(Matrix<? extends Number> matriz) {
+        if (this.ncols != matriz.getNrows()) {
+            throw new IllegalArgumentException(
+                "Columnas de A (" + this.ncols + ") no coinciden con Filas de B (" + matriz.getNrows() + ")."
+            );
+        }
+
+        Matrix<Double> resultado = new Matrix<>(this.nrows, matriz.getNcols());
+
+        // Algoritmo de multiplicación de matrices
+        for (int i = 0; i < this.nrows; i++) {           
+            for (int j = 0; j < matriz.getNcols(); j++) {   
+                for (int k = 0; k < this.nrows; k++) { 
+                    resultado.values[i][j] = resultado.values[i][j].doubleValue() + 
+                                            this.values[i][k].doubleValue() * matriz.values[k][j].doubleValue();
+                }
+            }
+        }
+
+        return resultado;
+    } // final de multiplicar
+
+
+    public Matrix<Double> multiplicarEscalar(double escalar) {
+
+        Matrix<Double> resultado = new Matrix<>(this.nrows, this.ncols);
+
+        for (int i = 0; i < this.nrows; i++) {
+            for (int j = 0; j < this.ncols; j++) {
+   
+                resultado.setValue(i, j, escalar * this.values[i][j].doubleValue());
+            }
+        }
+
+        return resultado;
+    }
+
+
+
+    public Matrix<T> transpuesta() {
+
+        Matrix<T> resultado = new Matrix<>(this.ncols, this.nrows);
+
+        for (int i = 0; i < nrows; i++) {
+            for (int j = 0; j < ncols; j++) {
+                resultado.values[j][i] = this.values[i][j];
+            }
+        }
+        return resultado;
+    }
+
+
+
+    private void validarIndices(int row, int col) {
+        if (row < 0 || row >= this.nrows || col < 0 || col >= this.ncols) {
+            throw new IndexOutOfBoundsException("Índice fuera de rango");
+        }
+    }
+
+
+
+    // Metodos Canonicos
+
+    @Override
+    public boolean equals(Object obj){
+        if (this == obj) return true;  // Todo es igual a si mismo
+        if (obj == null) return false; // NADA es igual a null
+
+        if (! (obj instanceof Matrix)) return false; // para ver si son el mismo tipo de instancia
+
+        Matrix<?> matriz = (Matrix<?>) obj;
+
+        if (this.nrows != matriz.getNrows() || this.ncols != matriz.getNcols()) {
+            return false;
+        }
+
+        // Si llegamos hasta aquí es porque si tenemos el mismo tipo de Matriz
+        for (int i = 0; i < this.nrows; i++) {
+            for (int j = 0; j < this.ncols; j++) {
+                if (this.values[i][j].doubleValue() != matriz.values[i][j].doubleValue()) {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+        
+    }
+
+    @Override
     public String toString(){
-        //TODO implementar esta mamada
-        return "";
+        String matriz = "";
+
+        for (int i = 0; i < nrows; i++) {
+            matriz += "[ ";
+            for (int j = 0; j < ncols; j++) {
+                matriz += values[i][j] + "\t";
+            }
+
+            matriz += "]\n";
+        }
+
+        return matriz;
+    }
+
+    @Override
+    public Object clone(){
+        Matrix<Double> matriz = new Matrix<>(this.nrows, this.ncols);
+
+        for (int i = 0; i < nrows; i++) {
+            for (int j = 0; j < ncols; j++) {
+                matriz.values[i][j] = getValue(i, j);
+            }
+        }
+
+
+        return matriz;
     }
     
-}
+}  // final de la clase
--- lib/src/tests/TestMatrix.java
+++ lib/src/tests/TestMatrix.java
@@ -1,5 +1,26 @@
 package tests;
 
-public class TestMatrix {
+import linearalgebra.Matrix;
 
+public class TestMatrix {
+    public static void main(String[] args) {
+
+        Matrix<Integer> m = new Matrix<>(2, 4);
+        System.out.println(m);
+        
+        Integer[][] valores = {{1 , 2, 3, 4} , {1 , 2,343,23}};
+
+        Matrix<Integer> m1 = new Matrix<>(valores);
+        System.out.println(m1);
+
+        System.out.println(m.sumar(m1));
+
+        System.out.println(m1.transpuesta());
+
+        System.out.println(m1.multiplicarEscalar(0));
+
+        System.out.println(m1.multiplicarEscalar(2));
+
+        System.out.println(m1.clone());
+    }
 }
